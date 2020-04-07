a6e5f065946c227c0f5ad1432fd1916360002a4d
Ebru Serdar Gülce ve Bitliler <evimiz@Ebru-MacBook-Pro.local>
2020-04-07 13:34:44 +0300

    commit for: React 02.2 - Components: dynamic rendering with props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+        name="Shelly Jennings"
+        avatar="https://randomuser.me/api/portraits/women/40.jpg"
+        online="true"
+      />
+      <Contact
+        name="Travis Payne"
+        avatar="https://randomuser.me/api/portraits/men/0.jpg"
+        online=""
+      />
+      <Contact
+        name="Gregory Butler"
+        avatar="https://randomuser.me/api/portraits/men/76.jpg"
+        online="true"
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./styles.css";
 
-function Contact() {
-  const name = "Shelly Jennings";
-  const avatar = "https://randomuser.me/api/portraits/women/40.jpg";
-  const online = true;
+function Contact(props) {
+  let name;
+  let avatar;
+  let online = { props };
   return (
     <div className="Contact">
-      <img className="avatar" src={avatar} />
+      <img className="avatar" src={props.avatar} />
       <div className="status">
-        <h6 className="name">{name}</h6>
-        <span className={online ? "status-online" : "status-offline"}></span>
-        <p className="status-text">{online ? "online" : "offline"}</p>
+        <h6 className="name">{props.name}</h6>
+        <span
+          className={props.online ? "status-online" : "status-offline"}
+        ></span>
+        <p className="status-text">{props.online ? "online" : "offline"}</p>
       </div>
     </div>
   );
 }
-
+Contact.propTypes = {
+  online: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+};
 export default Contact;
