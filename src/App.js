4ba126d3a326e2f623e18ef6901fd6975ccb84fc
Ebru Serdar Gülce ve Bitliler <evimiz@Ebru-MacBook-Pro.local>
2020-04-09 12:44:12 +0300

    second commit for React 03 -  displaying list of components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Shelly Jennings"
-        avatar="https://randomuser.me/api/portraits/women/40.jpg"
-        online="true"
-      />
-      <Contact
-        name="Travis Payne"
-        avatar="https://randomuser.me/api/portraits/men/0.jpg"
-        online=""
-      />
-      <Contact
-        name="Gregory Butler"
-        avatar="https://randomuser.me/api/portraits/men/76.jpg"
-        online="true"
-      />
+      <ContactList />
     </div>
   );
 }
