01b2209b5111e73d99af4a9d5804dfea6447723b
Ebru Serdar Gülce ve Bitliler <evimiz@Ebru-MacBook-Pro.local>
2020-04-14 11:49:06 +0300

    commit for React 05 - State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./styles.css";
 
-function Contact(props) {
-  let name;
-  let avatar;
-  let online = { props };
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} />
-      <div className="status">
-        <h6 className="name">{props.name}</h6>
-        <span
-          className={props.online ? "status-online" : "status-offline"}
-        ></span>
-        <p className="status-text">{props.online ? "online" : "offline"}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.name = name;
+    // this.avatar = avatar;
+    // this.pnline = online;
+    this.state = {
+      online: this.props.online,
+    };
+  }
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} />
+        <div className="status">
+          <h6 className="name">{this.props.name}</h6>
+          <span
+            className={this.state.online ? "status-online" : "status-offline"}
+            onClick={(event) => {
+              const newOnline = !this.state.online;
+              this.setState({ online: newOnline });
+            }}
+            // className={this.props.online ? "status-online" : "status-offline"}
+          ></span>
+          <p className="status-text">
+            {this.props.online ? "online" : "offline"}
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 Contact.propTypes = {
   online: PropTypes.bool.isRequired,
